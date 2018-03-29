bc5c211c168d7c81a723a33137d367d8c012dfe9
boyofgreen <boyofgreen@gmail.com>
2018-03-29 13:43:55 -0500

    init commit

---
+++ App1/bin/x86/Debug/AppX/js/default.js
@@ -0,0 +1,15 @@
+﻿
+
+document.getElementById('toast').addEventListener('click', function () {
+    if (window.CommunicationWinRT) {
+        testToast.toastMessage(document.getElementById('input').value, 0)
+    } else {
+        alert(document.getElementById('input').value)
+    }
+    })
+
+if (window.CommunicationWinRT) {
+    var testToast = new CommunicationWinRT.CommunicationWinRT();
+    var testCamera = new CommunicationWinRT.cameraWinRT();
+
+}
